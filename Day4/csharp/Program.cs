532fb25f946340752e77849da727f2cc43fe0c3d
2BitSalute <2BitSalute@github>
2024-12-03 22:19:21 -0800

    Remove redundant test code

--- Day4/csharp/Program.cs
+++ Day4/csharp/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// var filename = "../short_input";
-var filename = "../input";
+var filename = "../short_input";
+// var filename = "../input";
 
 var matrix = File.ReadAllLines(filename);
 var numC = matrix[0].Length;
@@ -13,20 +13,24 @@ var numR = matrix.Length;
 // - Vertical in 4 directions
 
 // Skip X
-var word = new[] { 'M', 'A', 'S' };
+var word = new [] { 'M', 'A', 'S' };
+
+bool IsInvalidRow(int indexR) => indexR >= numR || indexR < 0;
+bool IsInvalidCol(int indexC) => indexC >= numC || indexC < 0;
+
 
 int FindInDirection(int r, int c, int dirR, int dirC)
 {
     for(int i = 0; i < word.Length; i++)
     {
         var indexR = r + (i * dirR) + dirR;
-        if (indexR >= numR || indexR < 0)
+        if (IsInvalidRow(indexR))
         {
             return 0;
         }
 
         var indexC = c + (i * dirC) + dirC;
-        if (indexC >= numC || indexC < 0)
+        if (IsInvalidCol(indexC))
         {
             return 0;
         }
@@ -42,7 +46,7 @@ int FindInDirection(int r, int c, int dirR, int dirC)
 
 int Find(int r, int c)
 {
-    var dirs = new int [] { -1, 0, 1 };
+    var dirs = new [] { -1, 0, 1 };
 
     var result = 0;
     for (int dirR = 0; dirR < dirs.Length; dirR++)
@@ -56,16 +60,63 @@ int Find(int r, int c)
     return result;
 }
 
-var result = 0;
+bool FindXChar(int r, int c, int dirR, int dirC, char toFind)
+{
+    // M
+    var indexR = r + dirR;
+    if (IsInvalidRow(indexR))
+    {
+        return false;
+    }
+
+    var indexC = c + dirC;
+    if (IsInvalidCol(indexC))
+    {
+        return false;
+    }
+
+    return matrix[indexR][indexC] == toFind;
+}
+
+bool FindXHalf(int r, int c, int dirR, int dirC)
+{
+    return
+        FindXChar(r, c, dirR, dirC, 'M') &&
+        FindXChar(r, c, dirR * -1, dirC * -1, 'S');
+}
+
+int FindX(int r, int c)
+{
+    var dirs = new [] { 1, -1 };
+
+    var result = 0;
+    for (int dirR = 0; dirR < dirs.Length; dirR++)
+    {
+        for (int dirC = 0; dirC < dirs.Length; dirC++)
+        {
+            result += FindXHalf(r, c, dirs[dirR], dirs[dirC]) ? 1 : 0;
+        }
+    }
+
+    return result == 2 ? 1 : 0;
+}
+
+var part1Result = 0;
+var part2Result = 0;
 for (int r = 0; r < numR; r++)
 {
     for (int c = 0; c < numC; c++)
     {
         if (matrix[r][c] == 'X')
         {
-            result += Find(r, c);
+            part1Result += Find(r, c);
+        }
+        else if (matrix[r][c] == 'A')
+        {
+            part2Result += FindX(r, c);
         }
     }
 }
 
-Console.WriteLine($"Result: {result}");
+Console.WriteLine($"Part 1 result: {part1Result}");
+Console.WriteLine($"Part 2 result: {part2Result}");
