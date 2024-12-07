a3b981b306b8efe9ec2813c211b37201ef22e4b4
2BitSalute <2BitSalute@github>
2024-12-06 22:51:48 -0800

    Tidy

--- Day7/csharp/Program.cs
+++ Day7/csharp/Program.cs
@@ -1,4 +1,6 @@
-﻿// var filename = "../short_input";
+﻿using Op = System.Func<long, long, long>;
+
+// var filename = "../short_input";
 var filename = "../input";
 
 var lines = File.ReadAllLines(filename);
@@ -7,49 +9,42 @@ long Add(long a, long b) => a + b;
 long Mul(long a, long b) => a * b;
 long Con(long a, long b) => long.Parse(a.ToString() + b.ToString());
 
-long FindSolution(long target, long result, Func<long, long, long> op, long[] operands)
+long Find(long target, long result, long[] operands)
+{
+    operands = operands[1..];
+
+    long max = 0;
+    foreach(var op in new Op[] { Add, Mul, Con })
+    {
+        max = Math.Max(max, FindSolution(target, result, op, operands));
+    }
+
+    return max;
+}
+
+long FindSolution(long target, long result, Op op, long[] operands)
 {
     var operand = operands[0];
-
     result = op(result, operand);
-
     if (operands.Length == 1) // last operand
     {
         return result == target ? result : 0;
     }
 
-    operands = operands[1..];
-
-    return 
-        Math.Max(
-            FindSolution(target, result, Con, operands),
-            Math.Max(
-                FindSolution(target, result, Add, operands),
-                FindSolution(target, result, Mul, operands)
-            ));
+    return Find(target, result, operands);
 }
 
 var sum = 0L;
 foreach(var line in lines)
 {
     var colon = line.IndexOf(':');
-    var result = long.Parse(line[..colon]);
+    var target = long.Parse(line[..colon]);
     var operands =
         line[(colon + 1)..].Split(' ', StringSplitOptions.RemoveEmptyEntries)
         .Select(long.Parse)
         .ToArray() ?? throw new InvalidOperationException();
 
-    var operand = operands[0];
-    operands = operands[1..];
-
-    var s = Math.Max(
-        FindSolution(result, operand, Con, operands),
-        Math.Max(
-            FindSolution(result, operand, Add, operands),
-            FindSolution(result, operand, Mul, operands)
-        ));
-
-    sum += s;
+    sum += Find(target, result: operands[0], operands);
 }
 
 Console.WriteLine($"Solution: {sum}");
