31fe4b8ccdc00224d569244cb2ec628a62f94793
2BitSalute <2BitSalute@github>
2024-12-08 22:34:39 -0800

    Part 2 Day 9

--- Day9/csharp/Program.cs
+++ Day9/csharp/Program.cs
@@ -1,27 +1,7 @@
-﻿using System.Runtime.ExceptionServices;
-
-// var filename = @"../short_input";
+﻿// var filename = @"../short_input";
 var filename = @"../input";
 
 var lines = File.ReadAllLines(filename);
-
-int ParseChar(char c)
-{
-    return c switch
-    {
-        '0' => 0,
-        '1' => 1,
-        '2' => 2,
-        '3' => 3,
-        '4' => 4,
-        '5' => 5,
-        '6' => 6,
-        '7' => 7,
-        '8' => 8,
-        '9' => 9,
-        _ => throw new ArgumentException("Expecting a digit")
-    };
-}
 
 var file = true;
 var fs = new List<int>();
@@ -34,38 +14,85 @@ void Fill(int count, int value)
     }
 }
 
+var free = new List<(int L, int Pos)>();
+var files = new List<(int Id, int L, int Pos)>();
 var id = 0;
 
-foreach(var d in lines[0].Select(ParseChar)) // int.Parse(c + "")
+foreach(var length in lines[0].Select(c => int.Parse(c + "")))
 {
-    // alternating files and free space
     var value = file ? id : -1;
-    Fill(d, value);
+    var position = fs.Count;
 
-    id = file ? id + 1 : id;
+    if (file)
+    {
+        files.Add((id, length, position));
+        id += 1;
+    }
+    else
+    {
+        free.Add((length, position));
+    }
+
+    Fill(length, value);
 
     file = !file;
 }
 
-(int left, int right) = (0, fs.Count - 1);
-while(left < right)
+void Part2()
 {
-    if (fs[left] != -1)
+    for(int i = files.Count - 1; i >= 0; i--)
     {
-        left++;
-    }
-    else // found empty space
-    {
-        if (fs[right] == -1)
+        var file = files[i];
+        for (int j = 0; j < free.Count; j++)
         {
-            right--;
-        }
-        else // found something to move
-        {
-            (fs[left], fs[right]) = (fs[right], fs[left]);
+            var slot = free[j];
+            if (file.Pos < slot.Pos)
+            {
+                break;
+            }
+
+            if (slot.L >= file.L)
+            {
+                // swap file and free space
+                for(int k = 0; k < file.L; k++)
+                {
+                    (fs[slot.Pos + k], fs[file.Pos + k]) = (fs[file.Pos + k], fs[slot.Pos + k]);
+                }
+
+                // update free space at j to new length
+                free[j] = (slot.L - file.L, slot.Pos + file.L);
+
+                break;
+            }
         }
     }
 }
+
+void Part1()
+{
+    (int left, int right) = (0, fs.Count - 1);
+    while(left < right)
+    {
+        if (fs[left] != -1)
+        {
+            left++;
+        }
+        else // found empty space
+        {
+            if (fs[right] == -1)
+            {
+                right--;
+            }
+            else // found something to move
+            {
+                (fs[left], fs[right]) = (fs[right], fs[left]);
+            }
+        }
+    }
+}
+
+// Part1();
+Part2();
 
 var checksum = 0L;
 for(int i = 0; i < fs.Count; i++)
@@ -77,7 +104,8 @@ for(int i = 0; i < fs.Count; i++)
         checksum += value * i;
     }
 
-    // Console.Write(value);
+    // Console.Write(value != -1 ? value.ToString() : ".");
 }
+Console.WriteLine();
 
 Console.WriteLine($"Checksum: {checksum}");
